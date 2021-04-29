fc418873687b641ba2521be303e40ddfa7ca9273
Aliaksei Anishchanka <amsure00dev@gmail.com>
2021-04-29 18:41:36 +0200

    feat: two activities with navigation bar: list; calendar

--- app/src/main/java/pl/alex/pam_new/Calendar.java
+++ app/src/main/java/pl/alex/pam_new/Calendar.java
@@ -2,9 +2,6 @@ package pl.alex.pam_new;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,37 +9,25 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    RecyclerAdapter adapter;
+public class Calendar extends AppCompatActivity {
 
-    String[] months = {"Bali", "Bali", "Bali", "Bali", "Bali", "Bali", "Bali", "Bali"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new RecyclerAdapter(this, months);
-        recyclerView.setAdapter(adapter);
+        setContentView(R.layout.activity_calendar);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        MenuItem calendar = (bottomNavigationView).getMenu().findItem(R.id.list);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.calendar:
-                        Intent intent = new Intent(getBaseContext(), Calendar.class);
+                    case R.id.list:
+                        Intent intent = new Intent(getBaseContext(), MainActivity.class);
                         startActivity(intent);
                         finish();
                 }
                 return true;
             }
         });
-        calendar.setChecked(true);
-
-
     }
 }
